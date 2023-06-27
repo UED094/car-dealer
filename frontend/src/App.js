5cdedaf3b29c14efe29a1bba512f4d24dff4a225
Ugur Emre Dogan <ue.dogan@gmail.com>
2023-06-27 00:12:20 -0700

    Sign up page added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 
 function App() {
   return (
     <div>
       <h1> Hello from react!</h1>
+        <SignIn />
+        <SignUp />
     </div>
   );
 }
