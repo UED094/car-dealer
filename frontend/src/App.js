3e4bd164e4757dfab281141fc0e7ed4affef3951
Ugur Emre Dogan <ue.dogan@gmail.com>
2023-06-27 00:26:32 -0700

    Removed sign in and sign up page from the app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,13 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 
 function App() {
   return (
     <div>
-        <SignIn />
-        <SignUp />
+        <Home />
     </div>
   );
 }
